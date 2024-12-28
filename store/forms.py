4543eeac93eee621ade753043f7b6215b95d729a
Athira-K-Thilak <athirakthilak7@gmail.com>
2024-12-28 19:15:40 +0530

    inital commit

---
+++ store/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+class BookForm(forms.Form):
+
+    title=forms.CharField()
+
+    author=forms.CharField()
+
+    price=forms.IntegerField()
+
+    language=forms.CharField()
+
+    genre=forms.CharField()
+
+    year=forms.CharField()
---
+++ store/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+#Book[id,title,author,price,language,genre,year]
+
+class Book(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    author=models.CharField(max_length=250)
+
+    price=models.PositiveIntegerField()
+
+    language=models.CharField(max_length=200)
+
+    genre=models.CharField(max_length=100)
+
+    year=models.CharField(max_length=10)
